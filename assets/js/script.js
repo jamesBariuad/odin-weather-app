00372bbe01150b9e6b13dded87993f2a16abc152
jokandro <114486039+jokandro@users.noreply.github.com>
2023-09-05 09:56:17 +0800

    Code Refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,75 @@
 const form = document.querySelector("form");
 const apiKey = "1bebb0f7f9034991b1622653230309";
 
-const getData = async (searchLocation = "cabarroguis") => {
+//   try {
+//     const response = await fetch(
+//       `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchLocation}&days=1&aqi=no`
+//     );
+//     const responseData = await response.json();
+//     let data = {
+//       condition: responseData.current.condition.text,
+//       locationName: responseData.location.name,
+//       locationRegion: responseData.location.region,
+//       currentTempC: responseData.current.temp_c,
+//       feelsLikeC: responseData.current.feelslike_c,
+//       currentTempF: responseData.current.temp_f,
+//       feelsLikeF: responseData.current.feelslike_f,
+//       currentHumidity: responseData.current.humidity,
+//       chanceOfRain:
+//         responseData.forecast.forecastday[0].day.daily_chance_of_rain,
+//       weatherIcon: responseData.current.condition.icon,
+//       windSpeedKph: responseData.current.wind_kph,
+//       windSpeedMph: responseData.current.wind_mph,
+//     };
+//     return data;
+//   } catch (error) {
+//     alert("Please input a valid location");
+//   }
+// };
+const fetchData = async (searchLocation = "cabarroguis") => {
   try {
     const response = await fetch(
       `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchLocation}&days=1&aqi=no`
     );
-    const responseData = await response.json();
-    let data = {
-      condition: responseData.current.condition.text,
-      locationName: responseData.location.name,
-      locationRegion: responseData.location.region,
-      currentTempC: responseData.current.temp_c,
-      feelsLikeC: responseData.current.feelslike_c,
-      currentTempF: responseData.current.temp_f,
-      feelsLikeF: responseData.current.feelslike_f,
-      currentHumidity: responseData.current.humidity,
-      chanceOfRain:
-        responseData.forecast.forecastday[0].day.daily_chance_of_rain,
-      weatherIcon: responseData.current.condition.icon,
-      windSpeedKph: responseData.current.wind_kph,
-      windSpeedMph: responseData.current.wind_mph,
-    };
-    return data;
+    return await response.json();
   } catch (error) {
-    alert("Please input a valid location");
+    throw new Error("Please input a valid location");
   }
 };
 
-const displayData = async (neededData) => {
-  try {
-    const data = await neededData;
-    const condition = document.querySelector("#condition");
-    const location = document.querySelector("#location");
-    const date = document.querySelector("#date");
-    const time = document.querySelector("#time");
-    const temp = document.querySelector("#temp");
-    const icon = document.querySelector("#icon");
-    let tempSymbol = "°C";
+const extractData = (responseData) => {
+  const data = {
+    condition: responseData.current.condition.text,
+    locationName: responseData.location.name,
+    locationRegion: responseData.location.region,
+    currentTempC: responseData.current.temp_c,
+    feelsLikeC: responseData.current.feelslike_c,
+    currentTempF: responseData.current.temp_f,
+    feelsLikeF: responseData.current.feelslike_f,
+    currentHumidity: responseData.current.humidity,
+    chanceOfRain: responseData.forecast.forecastday[0].day.daily_chance_of_rain,
+    weatherIcon: responseData.current.condition.icon,
+    windSpeedKph: responseData.current.wind_kph,
+    windSpeedMph: responseData.current.wind_mph,
+  };
+  return data;
+};
 
-    condition.textContent = data.condition;
-    location.textContent = `${data.locationName}, ${data.locationRegion}`;
-    date.textContent = new Date().toDateString();
-    time.textContent = new Date().toLocaleTimeString();
-    temp.textContent = `${data.currentTempC}${tempSymbol} feels like ${data.feelsLikeC}${tempSymbol}`;
-    icon.src = data.weatherIcon;
-  } catch (error) {
-    return;
-  }
+const displayData = (data) => {
+  const condition = document.querySelector("#condition");
+  const location = document.querySelector("#location");
+  const date = document.querySelector("#date");
+  const time = document.querySelector("#time");
+  const temp = document.querySelector("#temp");
+  const icon = document.querySelector("#icon");
+  const tempSymbol = "°C";
+
+  condition.textContent = data.condition;
+  location.textContent = `${data.locationName}, ${data.locationRegion}`;
+  date.textContent = new Date().toDateString();
+  time.textContent = new Date().toLocaleTimeString();
+  temp.textContent = `${data.currentTempC}${tempSymbol} feels like ${data.feelsLikeC}${tempSymbol}`;
+  icon.src = data.weatherIcon;
 };
 
 const handleSubmit = async (e) => {
@@ -55,11 +77,26 @@ const handleSubmit = async (e) => {
   const loadingGif = document.querySelector(".loading.hidden");
   const location = document.querySelector("input").value;
 
-  loadingGif.classList.toggle("hidden");
-  const data = await getData(location);
-  loadingGif.classList.toggle("hidden");
-  displayData(data);
+  try {
+    form.reset();
+    loadingGif.classList.toggle("hidden");
+    const responseData = await fetchData(location);
+    const data = extractData(responseData);
+    displayData(data);
+  } catch (error) {
+    alert("please enter a valid location!");
+  } finally {
+    loadingGif.classList.toggle("hidden");
+  }
 };
 
 form.addEventListener("submit", (e) => handleSubmit(e));
-displayData(getData());
+(async () => {
+  try {
+    const initialData = await fetchData();
+    const data = extractData(initialData);
+    displayData(data);
+  } catch (error) {
+    alert("please enter a valid location!");
+  }
+})();
